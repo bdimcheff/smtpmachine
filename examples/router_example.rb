42354c94fc232d1a5eabedf4c6811f00d8b9544f
Brandon Dimcheff <bdimchef-git@wieldim.com>
2009-10-17 22:53:59 -0400

    basic functional router

--- examples/router_example.rb
+++ examples/router_example.rb
@@ -1,5 +1,37 @@
 require 'example_helper'
 
-describe 'SMTPMachine::Router' do  
+describe 'SMTPMachine::Router' do
+  before(:each) do
+    @router = Class.new do
+      include SMTPMachine::Router
+    end
+  end
   
+  it 'has a map method' do
+    @router.should respond_to(:map)
+  end
+  
+  it 'maps all addresses' do
+    mapped = false
+    
+    @router.map(/.*/) { mapped = true }
+    @router.new.route!('foo@example.com')
+    
+    mapped.should be_true
+  end
+
+  it 'allows methods to be called on the including object' do
+    @router.class_eval do
+      def foo
+        true
+      end
+    end
+    
+    called = false
+
+    @router.map(/.*/) { called = true }
+    @router.new.route!('')
+
+    called.should be_true
+  end
 end
--- lib/smtpmachine/router.rb
+++ lib/smtpmachine/router.rb
@@ -1,5 +1,31 @@
 module SMTPMachine
-  class Router
+  module Router
+    def self.included(base) # :nodoc:
+      base.extend ClassMethods
+    end
     
+    module ClassMethods
+      attr_reader :routes
+
+      def map(regex, options = {}, &block)
+        define_method "__email #{regex}", &block
+        unbound_method = instance_method("__email #{regex}")
+
+        block = lambda { unbound_method.bind(self).call }
+        
+        @routes ||= []
+        @routes.push([regex, block]).last
+      end
+    end
+
+    def route!(email)
+      routes = self.class.routes
+
+      routes.each do |regex, block|
+        if regex =~ email
+          instance_eval(&block)
+        end
+      end
+    end
   end
 end
